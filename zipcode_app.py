6418a794f0ba87ef3a3502a8d533fe4f6008ba0c
Leonardo Larsen Rocha <leonardo.larsen@ambevtech.com.br>
2021-04-21 16:53:29 -0300

    ZipCode App

---
+++ zipcode_app.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+
+from zipcode_db import ZipCodeDB
+from zipcode_dto import ZipCodeDTO
+
+
+class ZipCodeApp(FastAPI):
+
+    def index(self):
+        return {"app_name": "Zip Code App"}    
+
+    def hc(self):
+        return {"status": "OK"}
+
+    def get_zip_code(self, zipcode=None):
+        if(zipcode is None or len(zipcode) != 8):
+            message = 'Zip Code must contain 8 characters'
+            raise cherrypy.HTTPError(400, message=message)
+
+        zip_code = self.zip_code_fromdb(zipcode)
+
+        if zip_code:
+            return zip_code
+        else:
+            return(f'Zip code {zipcode} does not exist.')
+
+    def zip_code_fromdb(self, param=''):
+
+        db = ZipCodeDB('base/cep.db')
+        zip_codes = db.select_where(f"cep = '{param}'")
+        db.close()
+
+        result = []
+        for zipcode in zip_codes:
+            result.append(f"{ZipCodeDTO(zipcode).to_dict()}")
+        return result
