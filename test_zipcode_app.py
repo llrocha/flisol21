b83d974154afe5c5e5ae23b7e230ff3b39bbfeab
Leonardo Larsen Rocha <leonardo.larsen@ambevtech.com.br>
2021-04-23 18:31:53 -0300

    Change function name *_cep to *_zipcode

--- test_zipcode_app.py
+++ test_zipcode_app.py
@@ -19,22 +19,22 @@ def test_index():
     assert response.status_code == 200
     assert response.json() == {"app_name": "Zip Code App"}
 
-def test_empty_cep():
+def test_empty_zipcode():
     response = client.get("/zipcode")
     assert response.status_code == 404
     assert response.json() == {"detail": "Not Found"}
 
-def test_invalid_cep():
+def test_invalid_zipcode():
     response = client.get("/zipcode/12345")
     assert response.status_code == 404
     assert response.json() == {"detail": "Zip Code must contain 8 characters"}
 
-def test_unexistent_cep():
+def test_unexistent_zipcode():
     response = client.get("/zipcode/12345678")
     assert response.status_code == 404
     assert response.json() == {"detail":"Zip code 12345678 does not exist."}
 
-def test_existent_cep():
+def test_existent_zipcode():
     response = client.get("/zipcode/89035300")
     assert response.status_code == 200
     assert response.json() == ["{'id': 89035300, 'zipcode': '89035300', 'city': 'Blumenau', 'state': 'SC', 'neighborhood': 'Vila Nova', 'public_place': 'Rua Theodoro Holtrup', 'description': ''}"]
